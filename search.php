d7bce701f8c86527a4c4ccf6b719e7e2967407a2
liwei <498129372@qq.com>
2019-01-04 15:48:28 +0800

    天灵宝燕官网首次上传

---
+++ search.php
@@ -0,0 +1,27 @@
+<?php
+header("Content-type:text/html;charset=utf-8");
+$code = $_GET['FwCode'];
+$info = file_get_contents('http://www.12365.cm/fwqueryjson.ashx?FwCode='.$code);
+//$info = trim($info,'(');
+//$info = trim($info,')');
+//echo $info;exit;
+//echo json_decode($info,true);
+if(preg_match("/QueryResult\":\".+?\"/", $info, $matches)){
+	$msg = $matches[0];
+	$msg = trim($msg,'QueryResult":"');
+	$msg = trim($msg,'"');
+}else $msg = '查询失败';
+$return = [
+	'code' => 1,
+	'msg' => $msg
+];
+echo json_encode($return);
+/*if (strpos($info,'防伪已经被冻结')) {
+	$return = [
+		'code' => -1,
+		'msg' => '您好，您所查询的防伪已经被冻结！'
+	];
+	echo json_encode($return);
+}else{
+	echo $info;
+}*/
