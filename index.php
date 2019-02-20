e5e47cd0d5d0c86f4fc77cf333a7ecf36d87ad54
liwei <498129372@qq.com>
2019-02-20 17:51:06 +0800

    修改

--- index.php
+++ index.php
@@ -2,7 +2,6 @@
 require_once "jssdk.php";
 $jssdk = new JSSDK("wxe6ada8c3aae3c0ee", "d13a3343a7bd45b69a8c82ddeaa4785b");
 $signPackage = $jssdk->GetSignPackage();
-print_r($signPackage);exit;
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -389,7 +388,7 @@ print_r($signPackage);exit;
     	wx.config({
 		    debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
 		    appId: '<?php echo $signPackage["appId"];?>',
-		    timestamp: '<?php echo $signPackage["timestamp"];?>',
+		    timestamp: <?php echo $signPackage["timestamp"];?>,
 		    nonceStr: '<?php echo $signPackage["nonceStr"];?>',
 		    signature: '<?php echo $signPackage["signature"];?>',
 		    jsApiList: [
