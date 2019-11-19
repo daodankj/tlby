bf6c370badca6bbf81617318167f8c62b8fab3ac
liwei <498129372@qq.com>
2019-11-19 14:06:26 +0800

    新官网提交

---
+++ index-b.php
@@ -0,0 +1,420 @@
+<?php
+/*require_once "jssdk.php";
+$jssdk = new JSSDK("wxe6ada8c3aae3c0ee", "d13a3343a7bd45b69a8c82ddeaa4785b");
+$signPackage = $jssdk->GetSignPackage();*/
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>天灵宝燕官网 吃即食燕窝，找天灵宝燕</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="keywords" content="天灵宝燕官网 吃即食燕窝，找天灵宝燕，天灵宝燕，燕窝" />
+<meta name="Description" content="天灵宝燕，我们的产品理念，坚持使用原生态无污染的高级燕盏材料，确保每一盏都经过显微镜纯手工三挑三选古法挑毛工序，科学配比各营养成分保证每一瓶燕窝含量高达25%以上，拒绝制作过程使用添加剂，保证每一个成品都经过生产线超高温灭菌、全数控精准蒸煮，采用真空灌装，达到无菌标准，用心做出0添加承诺可以在常温保存的好吃燕窝">
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+<!-- Custom Theme files -->
+<link href="css/bootstrap.css" type="text/css" rel="stylesheet" media="all">
+<link href="css/style.css" type="text/css" rel="stylesheet" media="all">  
+<link href="css/font-awesome.css" rel="stylesheet">   <!-- font-awesome icons --> 
+<link rel="stylesheet" href="css/flexslider.css" type="text/css" media="screen" /> 
+<link rel="stylesheet" href="css/lightbox.css">  <!-- light-box -->
+<link href="images/logo.png" rel="shortcut icon">
+<!-- //Custom Theme files -->    
+<!-- web-fonts -->    
+<link href="http://fonts.googleapis.com/css?family=Tulpen+One" rel="stylesheet">
+<link href='http://fonts.googleapis.com/css?family=Roboto+Condensed:400,300,300italic,400italic,700,700italic' rel='stylesheet' type='text/css'>
+<style type="text/css">
+    
+	h3.w3title-agile {
+	    color: #c59474;
+	    font-size: 1.5rem;
+	    margin-bottom: 0.6em;
+	    text-align: center;
+	    text-transform: capitalize;
+	    letter-spacing: 4px;
+	    letter-spacing:0px;
+	} 
+	.wap{
+		color: #c59474;
+		font-size: 1.5rem;
+	}
+	.header {
+	   background-color: #fff;
+	}
+	.navbar-default .navbar-toggle .icon-bar {
+	    background-color: #c59474;
+	}
+	.navbar-default .navbar-toggle {
+	    border-color: #c59474;
+		margin: 2.4em 1em;
+	}
+	.welcome-img{
+		border: 0px;
+	}
+	@media only screen and (min-width: 1029px){
+		h3.w3title-agile {
+		    font-size: 3rem;
+		} 
+	}
+	@media only screen and (min-width: 1029px){
+		.wap {
+		    font-size: 3rem;
+		} 
+	}
+</style>
+<!-- //web-fonts -->  
+</head>
+<body> 
+	<!-- header -->
+	<div id="home" class="header">
+		<div class="container" style="height: 60px">
+			<div class="header-nav">
+				<nav class="navbar navbar-default">
+					<!-- Brand and toggle get grouped for better mobile display -->
+					<div class="navbar-header" style="height: 60px">
+					  <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" style="margin-top: 15px">
+						<span class="sr-only">Toggle navigation</span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					  </button>
+						<div class="logo" style="width: 55px">
+							<!-- <h1><a class="navbar-brand" href="index.html"><span>M</span> <span>T</span> <span>J</span><br><i>Packers & Movers</i></a></h1> -->
+							<img src="/images/logo.png" height="55px" style=" margin-top: 2px">
+						</div>
+					</div> 
+					<!-- Collect the nav links, forms, and other content for toggling -->
+					<div class="collapse navbar-collapse nav-wil" id="bs-example-navbar-collapse-1" style="background-color: #fff;">
+					 <ul class="nav navbar-nav">
+						<li class="hvr-sweep-to-bottom active"><a href="index.html">首页</a></li>
+						<li class="hvr-sweep-to-bottom"><a style="color: #000" href="#about" class="scroll">产品视频</a></li>
+						<li class="hvr-sweep-to-bottom"><a style="color: #000" href="#welcome" class="scroll">品牌介绍</a></li>
+						<li class="hvr-sweep-to-bottom"><a style="color: #000" href="#services" class="scroll">产品简介</a></li>
+						<li class="hvr-sweep-to-bottom"><a style="color: #000" href="#gallery" class="scroll">关注我们</a></li> 
+						<li class="hvr-sweep-to-bottom"><a style="color: #000" href="#contact" class="scroll">防伪查询</a></li>
+					  </ul>
+					</div><!-- /.navbar-collapse -->
+				</nav>
+			</div>
+		</div>
+	</div>
+	<!-- //header -->
+	<!-- banner -->
+	<div class="banner">  
+		<!-- banner-text -->
+		<div class="banner-text"> 
+			<!-- <h4><span class="glyphicon glyphicon-phone"></span> +11 222 333 444</h4> -->	
+			<div class="flexslider">
+				<ul class="slides">
+					<li>
+						<h5>天灵宝燕</h5>
+						<h6>1</h6>
+						<h2>爱，让时光驻足</h2>
+					</li>
+					<li>
+						<h5>天灵宝燕</h5>
+						<h6>1</h6>
+						<h3>龄秘鲜炖燕窝</h3>
+					</li>
+				</ul>
+			</div>    
+		</div> 
+		<!-- //banner-text -->   
+	</div>
+	<!-- //banner -->
+	<!-- about -->
+	<div id="about" class="about" style="background-color: #ffffff;">
+		<div class="container">
+			<!-- <h3 class="w3title-agile">产品视频</h3> -->  
+			<div class="about-tp">
+				<div class="col-md-12 about-agileits-w3layouts-left">
+					<!-- <iframe src=""></iframe> -->
+					<video style="max-width: 100%" controls  playsinline='' webkit-playsinline=''  src="tlby.mp4"
+                           width="100%"  preload="none"  poster="images/video.jpg">
+                    </video>
+				</div>
+				<!-- <div class="col-md-6 about-agileits-w3layouts-right">
+					<h4>Voluptatibus maiores  </h4>
+					<p>Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.</p>
+					<div class="bar_group bars-w3ls-row">
+						<div class='bar_group__bar thin' label='Ut Commodo' show_values='true' tooltip='true' value='350'></div>
+						<div class='bar_group__bar thin' label='Nulla Facilisi' show_values='true' tooltip='true' value='222'></div>
+						<div class='bar_group__bar thin' label='Lorem ipsum dolor' show_values='true' tooltip='true' value='450'></div>
+						<div class='bar_group__bar thin' label='Nam ex velit' show_values='true' tooltip='true' value='286'></div>
+					</div> 
+				</div> -->
+				<div class="clearfix"></div> 
+			</div>
+		</div>
+	</div>
+	<!-- //about-->
+
+	<!-- welcome -->
+	<div id="welcome" class="welcome" style="background-color: #ffffff">
+		<div class="container"> 
+			<h3 class="w3title-agile">品牌介绍</h3> 
+			<div class="w3l-welcome-info">
+				<div class="col-sm-6 col-xs-6 welcome-grids">
+					<div class="welcome-img">
+						<img src="images/img1.jpg" class="img-responsive zoom-img" alt="">
+					</div>
+				</div>
+				<div class="col-sm-6 col-xs-6 welcome-grids">
+					<div class="welcome-img">
+						<img src="images/img2.jpg" class="img-responsive zoom-img" alt="">
+					</div>
+				</div>
+				<div class="clearfix"> </div>
+			</div>
+			<div class="w3l-welcome-text">
+				<p style="text-align: left;">我们的灵感来自古人使用燕窝养生历史。燕窝是达官贵人的食品，到明代更列为宫廷御膳。明《本草纲目》记载，燕窝入肺生气、入肾滋水、入胃补脾、补而不燥。清康熙年的《调鼎集》记载的数十种“上席菜单”中，名列首位的就是燕窝。</p>
+
+				<p style="text-align: left;">我们的材料全部源于马来西亚。马来西亚地处印度洋和太平洋宽阔的海域，天然形成的热带雨林，常年雨水充沛，生物繁殖速度快，保证了金丝燕的食物供给。马来西亚的温度和湿度更适合金丝燕栖息，让燕子自由自在的快乐生活，才能产出最好的燕窝。</p>
+
+				<p style="text-align: left;">我们的产品理念，坚持使用原生态无污染的高级燕盏材料，确保每一盏都经过显微镜纯手工三挑三选古法挑毛工序，科学配比各营养成分保证每一瓶燕窝含量高达25%以上，拒绝制作过程使用添加剂，保证每一个成品都经过生产线超高温灭菌、全数控精准蒸煮，采用真空灌装，达到无菌标准，用心做出0添加承诺可以在常温保存的好吃燕窝。</p>
+
+				<p style="text-align: left;">我们的价值观：生命、美丽、爱。美丽的根本不需要修饰外在，而是身心愉悦的享受生活，发自内心的健康幸福的自然流露。</p>
+			</div>
+		</div>
+	</div>
+	<!-- //welcome -->
+	
+	<!-- services -->
+	<div id="services" class="welcome">
+		<div class="container"> 
+			<h3 class="w3title-agile">产品简介</h3> 
+			<div class="w3l-welcome-info">
+				<div class="col-sm-6 col-xs-6 welcome-grids">
+					<div class="welcome-img">
+						<img src="images/img3.jpg" class="img-responsive zoom-img" alt="">
+					</div>
+				</div>
+				<div class="col-sm-6 col-xs-6 welcome-grids">
+					<div class="welcome-img">
+						<img src="images/img4.jpg" class="img-responsive zoom-img" alt="">
+					</div>
+				</div>
+				<div class="clearfix"> </div>
+			</div>
+			<div class="w3l-welcome-text">
+				<p style="text-align: left;">燕窝中所含有的独特生物性活性蛋白分子，具有促进表皮细胞的再生；增加细胞诱发的免疫功能；增加个体对X放射线损害的抗御能力，这种活性蛋白质极易被人体吸收，燕窝既可调理人体内分泌，调节机能，也可强壮身体，滋润养颜，并可供给身体热量，促进脂肪代谢。</p>
+				<p style="text-align: left;">天灵宝燕即食燕窝，轻拧瓶盖，即可享受，营养美味，养颜更养人。</p><p style="text-align: left;">隐藏年龄的秘密武器，天灵宝燕·龄秘鲜炖燕窝就是这样的神奇，它可以将时间永远的定格在这一刻。</p>
+			</div>
+		</div>
+	</div>
+	<!-- //services -->
+	<!-- gallery -->
+	
+	<!-- //gallery --> 
+	<!-- news --> 
+	<div id="gallery" class="content-mid">
+		<img src="images/gzh.jpg" width="100%">
+		<div class="clearfix"></div>
+	</div>
+	<!-- //news -->
+	<!-- testimonials -->
+	<!-- <div id="news" class="testimonials">
+		<div class="container">
+			<h3 class="w3title-agile">我们的团队</h3> 
+			<div class="w3_testimonials_grids">
+				<section class="slider">
+					<div class="flexslider flexslider1">
+						<ul class="slides">
+							<li>	
+								<div class="w3_testimonials_grid">
+									<img src="images/1.png" alt=" " class="img-responsive" />
+									<h4><i>Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil 
+										impedit quo minus id quod maxime placeat facere possimus, omnis voluptas.</i></h4>
+									<h5>John Frank</h5>
+									<p>Founder</p>
+								</div>
+							</li>
+							<li>	
+								<div class="w3_testimonials_grid">
+									<img src="images/2.png" alt=" " class="img-responsive" />
+									<h4><i>Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil 
+										impedit quo minus id quod maxime placeat facere possimus, omnis voluptas.</i></h4>
+									<h5>Michael Doe</h5>
+									<p>Transport Agent</p>
+								</div>
+							</li>
+							<li>	
+								<div class="w3_testimonials_grid">
+									<img src="images/3.png" alt=" " class="img-responsive" />
+									<h4><i>Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil 
+										impedit quo minus id quod maxime placeat facere possimus, omnis voluptas.</i></h4>
+									<h5>Thomas Carl</h5>
+									<p>Transport Agent</p>
+								</div>
+							</li>
+						</ul>
+					</div>
+				</section>
+				 
+					<script type="text/javascript">
+					$(window).load(function(){
+					  $('.flexslider1').flexslider({
+						animation: "slide",
+						start: function(slider){
+						  $('body').removeClass('loading');
+						}
+					  });
+					});
+				  </script>
+				
+			</div>
+		</div>
+	</div> -->
+	<!-- //testimonials -->
+	<!-- news letter -->
+	<div id="contact" class="subscribe">
+		<div class="container">
+			<h3 class="w3title-agile2 wap">防伪查询</h3>
+			<p id="ReturnResult" style="font-size: 16px;"></p>
+			<!-- <div id="ReturnResult" style="font-size: 12px"> -->
+			<form onsubmit="return search();">
+				<input type="number" name="email" id="FwCode" placeholder="请输入防伪码查询" class="user" required="" style="border: 1px #ccc solid">
+				<input type="submit" id="btnQuery" value="马上查询">
+			</form>
+			<!-- <p class="spam">Copyright &copy; 2018.Company name All rights</p> -->
+		</div>
+
+	</div>
+	<p class="spam" style="text-align: center;">Copyright &copy; 2018. All rights  <a href="http://www.miitbeian.gov.cn">粤ICP备18016350号-1</a></p>
+	
+	<!-- //modal-about -->     
+	<!-- js --> 
+	<script src="js/jquery-2.2.3.min.js"></script>
+	<!-- FlexSlider --> 
+	<script defer src="js/jquery.flexslider.js"></script>
+	<script type="text/javascript">
+		$(window).load(function(){
+		  $('.flexslider').flexslider({
+			animation: "slide",
+			start: function(slider){
+			  $('body').removeClass('loading');
+			}
+		  });
+		});
+	</script>
+	<!-- End-slider-script -->
+	<script src="js/SmoothScroll.min.js"></script> 
+	<!-- start-smooth-scrolling -->
+	<script type="text/javascript" src="js/move-top.js"></script>
+	<script type="text/javascript" src="js/easing.js"></script>	
+	<script type="text/javascript">
+			jQuery(document).ready(function($) {
+				$(".scroll").click(function(event){		
+					event.preventDefault();
+			
+			$('html,body').animate({scrollTop:$(this.hash).offset().top},1000);
+				});
+			});
+	</script>
+	<!-- //end-smooth-scrolling -->	
+	<!-- smooth-scrolling-of-move-up -->
+	<script type="text/javascript">
+		$(document).ready(function() {
+			/*
+			var defaults = {
+				containerID: 'toTop', // fading element id
+				containerHoverID: 'toTopHover', // fading element hover id
+				scrollSpeed: 1200,
+				easingType: 'linear' 
+			};
+			*/
+			
+			$().UItoTop({ easingType: 'easeOutQuart' });
+			
+		});
+		function search(){
+			var $FwCode = $("#FwCode");
+			var $CheckResult = $("#ReturnResult");
+            var RegNumber = /^[0-9]*[1-9][0-9]*$/;
+            var flag = false;
+            $CheckResult.html("");
+            if ($FwCode.val().length == 0) {
+                $CheckResult.html("防伪码不能为空");
+                $FwCode.focus();
+                return false;
+            }else if ($FwCode.val().length < 10) {
+               $CheckResult.html("防伪码长度不能少于10位");	 
+               $FwCode.focus();
+               return false;
+            }else if ($FwCode.val().length > 24) {
+               $CheckResult.html("防伪码长度不能大于24位");
+               $FwCode.focus();
+               return false;
+            }else if (!RegNumber.test($FwCode.val())) {
+               $CheckResult.html("您输入的防伪码不是数字");
+               $FwCode.focus();
+               return false;
+            }
+           			
+            $CheckResult.html("正在查询......");
+
+            $(this).attr("disabled", true);
+            var $strurl="" ;
+            //var searchurl = "http://www.12365.cm/fwqueryjson.ashx?callback=?";
+            var searchurl = "search.php";
+            $.getJSON(searchurl,{ FwCode: $FwCode.val() },function(data) {
+
+              	//alert(data.CodeState);  //CodeState 为查询状况：0 错误 1 正确; 2 重复  
+
+             	//alert(data.QueryResult) ;   //QueryResult 查询结果 
+             	console.log(data);
+             	/*if (('code' in data) && data.code==1) {
+             		$CheckResult.html(data.msg);
+             	}else{
+             		$CheckResult.html(data.QueryResult);
+             	}*/
+             	$CheckResult.html(data.msg);
+                setTimeout(btnEnabled, 2000);
+            });
+			return false;
+		}
+		function btnEnabled() {
+            $("#btnQuery").attr("disabled", false);
+        }
+	</script>
+	<!-- //smooth-scrolling-of-move-up -->  
+	<script src="js/bars.js"></script> 
+	<!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="js/bootstrap.js"></script>
+    <script type="text/javascript" src="http://res.wx.qq.com/open/js/jweixin-1.4.0.js"></script>
+    <script>
+    	wx.config({
+		    debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+		    appId: '<?php echo $signPackage["appId"];?>',
+		    timestamp: <?php echo $signPackage["timestamp"];?>,
+		    nonceStr: '<?php echo $signPackage["nonceStr"];?>',
+		    signature: '<?php echo $signPackage["signature"];?>',
+		    jsApiList: [
+		    	'updateAppMessageShareData',
+		    	'updateTimelineShareData'
+		    ] // 必填，需要使用的JS接口列表
+		});
+		wx.ready(function () {   //需在用户可能点击分享按钮前就先调用
+		    wx.updateAppMessageShareData({ //分享给朋友
+		        title: '天灵宝燕', // 分享标题
+		        desc: '龄秘鲜炖燕窝。即食燕窝，轻拧瓶盖，即可享受，营养美味，养颜更养人', // 分享描述
+		        link: 'http://www.tlbyyw.com', // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+		        imgUrl: 'http://www.tlbyyw.com/sharelogo.png', // 分享图标
+		        success: function () {
+		          // 设置成功
+		        }
+		    });
+		    wx.updateTimelineShareData({ //分享朋友圈
+		        title: '天灵宝燕', // 分享标题
+		        link: 'http://www.tlbyyw.com', // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+		        imgUrl: 'http://www.tlbyyw.com/sharelogo.png', // 分享图标
+		        success: function () {
+		          // 设置成功
+		        }
+		    })
+		});
+    </script>
+</body>
+</html>
